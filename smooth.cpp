733a483845415d11a268a586b0445c00c262cbbb
Himanshu <him.star@yahoo.ca>
2014-05-17 06:19:32 +0000

    more functions definations

--- smooth.cpp
+++ smooth.cpp
@@ -20,3 +20,26 @@ pixel pixel:: operator*(double v) const
     temp.blue = blue*v;
     return temp;
 }
+
+pixel pixel:: operator/(int v) const
+{
+    pixel temp;
+    temp.red = red/v;
+    temp.green = green/v;
+    temp.blue = blue/v;
+    return temp;
+}
+
+ostream& operator<<(ostream& out, const pixel& p)
+{
+    out << p.red << ' ';
+    out << p.green << ' ';
+    out << p.blue;
+    return out;
+}
+
+istream& operator<<(istream& in, pixel& p)
+{
+    in >> p.red >> p.green >> p.blue;
+    return in;
+}
