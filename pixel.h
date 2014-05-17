39407af6703452d00d460d63eda47d1ac4441341
Himanshu <him.star@yahoo.ca>
2014-05-17 05:20:53 +0000

    First commit - pixel class file and a FALTU one

---
+++ pixel.h
@@ -0,0 +1,50 @@
+#!/usr/bin/cpp
+/*  Pixel class declaration
+    File name: pixel.h
+*/
+
+#ifndef PIXEL_H
+#define PIXEL_H
+#include <iostream>
+using namespace std:
+class pixel
+{
+public:
+//  Constructor Functions
+pixel(); // Initialize data members to zero
+pixel(int);
+pixel(int, int, int);
+
+//  Accessor Functions
+int getRed() const;
+int getGreen() const;
+int getBlue() const;
+
+//  Setter or Modifier Functions
+void setPixel_value(int r, int g, int b);
+void setBlue(int);
+void setGreen(int);
+
+//  Overloaded Operators
+
+//  Addition
+//  Add twoo pixel
+pixel operator+(pixel p) const;
+
+//  Multiplication
+//  Multiply a pixel by a floating point value
+pixel operator*(double v) const;
+
+//  Division
+//  Divide a pixel by an integer value
+pixel operator/(int v) const;
+
+//  Input operator
+friend istream& operator >>(istream& in, Pixel& p);
+
+//  Output Operator
+friend ostream& operator <<(ostream& out, Pixel p);
+private:
+unsigned int red, green, blue;
+};
+#endif
---
+++ smooth.cpp
@@ -0,0 +1,12 @@
+#!/usr/bin/cpp
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, I am Himanshu" << endl;
+
+    return 0;
+}
+// yo
