b99e65ff16327d0e24057a32912f30c137d47229
Himanshu <him.star@yahoo.ca>
2014-05-17 05:55:09 +0000

    changed smooth.cpp to define function prototypes of pixel class

--- smooth.cpp
+++ smooth.cpp
@@ -1,12 +1,22 @@
 #!/usr/bin/cpp
 #include <iostream>
-
+#include "pixel.h"
 using namespace std;
 
-int main()
+pixel pixel:: operator+(pixel p) const
 {
-    cout << "Hello, I am Himanshu" << endl;
-
-    return 0;
+    pixel temp;
+    temp.red = red + p.red;
+    temp.green = gree + p.green;
+    temp.blue = blue +p.blue;
+    return temp;
 }
-// yo
+
+pixel pixel:: operator*(double v) const
+{
+    pixel temp;
+    temp.red = red*v;
+    temp.green = green*v;
+    temp.blue = blue*v;
+    return temp;
+}
